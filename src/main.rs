168cd2b0448077288a3863c5aa28377f661ee917
LuoZhipeng <holyfight6@gmail.com>
2023-02-01 12:02:04 +0800

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use futures::executor::block_on;
+use wgpu_toutorial::state::State;
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
+fn main() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut state = block_on(State::new(&window));
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() && !state.input(event) => match event {
+            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            WindowEvent::KeyboardInput { input, .. } => match input {
+                KeyboardInput {
+                    state: ElementState::Pressed,
+                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                _ => {}
+            },
+            WindowEvent::Resized(new_size) => state.resize(*new_size),
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                state.resize(**new_inner_size)
+            }
+            _ => {}
+        },
+        Event::RedrawRequested(_) => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                Err(wgpu::SwapChainError::Lost) => state.recreate_swapchain(),
+                Err(wgpu::SwapChainError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+        Event::MainEventsCleared => window.request_redraw(),
+        _ => {}
+    });
+}
